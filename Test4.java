d14055f844eca88cb05a23d73d128d5da1ecfcee
gitHector1980 <vedder_30@hotmail.com>
2024-09-13 11:51:20 -0600

    Un archivo de nombre alreves mas, fue agregado

---
+++ Test4.java
@@ -0,0 +1,8 @@
+public class Test4 {
+    public static void main(String[] args) {
+        String n ="Gola";
+        for (int i = n.length()-1; i >=0; i--) {
+            System.out.print(n.charAt(i));
+        }
+    }    
+}

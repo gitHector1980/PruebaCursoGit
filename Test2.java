039bb58645f9fdba7f3ee860a17d59592a610fd3
gitHector1980 <vedder_30@hotmail.com>
2024-09-12 18:36:34 -0600

    Dos Archivos Java nuevos de nombres alrevés

---
+++ Test2.java
@@ -0,0 +1,13 @@
+
+
+public class Test2 {
+    public static void main(String[] args) {    
+    String name = "Paola";
+
+    for (int i = name.length()-1 ; i >=0 ; i--) 
+    {
+        System.out.print(name.charAt(i));         
+    }        
+    }
+}
+   
---
+++ Test3.java
@@ -0,0 +1,12 @@
+public class Test3 
+{
+    public static void main(String[] args) 
+    {
+        String s = "Mexico";
+        
+        for(int i = s.length()-1; i>=0; i--)
+        {
+            System.out.print(s.charAt(i));
+        }
+    }    
+}

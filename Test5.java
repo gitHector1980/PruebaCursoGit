deb41c64fee87c28bc0b747d298bad809f99a433
gitHector1980 <vedder_30@hotmail.com>
2024-09-26 17:55:04 -0600

    Otro ejercicio de nombre alrevez

---
+++ Test5.java
@@ -0,0 +1,9 @@
+public class Test5 {
+    public static void main(String[] args) {
+        String name = "Hector";
+        for (int i = name.length() - 1; i >= 0 - 1; i--) {
+            System.out.println(name.charAt(i));
+        }
+
+    }
+}

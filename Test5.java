1856cfafc1efa23a80ec3fa9e84f9231a436faa9
gitHector1980 <vedder_30@hotmail.com>
2024-09-26 18:30:26 -0600

    Mensaje de Hola paparrin

--- Test5.java
+++ Test5.java
@@ -1,10 +1,12 @@
 public class Test5 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         String name = "Hector";
-        for (int i = name.length() - 1; i >= 0 - 1; i--) {
-            System.out.println(name.charAt(i));
-            System.out.println("Hola Paparrin");
+        for (int i = name.length() - 1; i > 0 - 1; i--) 
+        {
+            System.out.println(name.charAt(i));            
         }
+        System.out.println("Hola Paparrin");
 
     }
 }
